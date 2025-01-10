4e8d9bc6cbaf48fa947767f195af842e796c1e55
Sunpil Kim <spkim8804@gmail.com>
2025-01-11 01:36:07 +0900

    ffmpeg added

--- utils/.ipynb_checkpoints/__init__-checkpoint.py
+++ utils/.ipynb_checkpoints/__init__-checkpoint.py
@@ -2,3 +2,4 @@ from .naming import get_unique_filename
 from .preprocessing import zero_replacing
 from .yolosaver import YoloSaver
 from .yolorunner import YoloRunner
+from .export_frame import export_frame_image, get_frame_types
--- utils/.ipynb_checkpoints/export_frame-checkpoint.py
+++ utils/.ipynb_checkpoints/export_frame-checkpoint.py
@@ -1,5 +1,30 @@
 import cv2
+import os
+import subprocess
 
 def export_frame_image(frame_data):
     frame, frame_id, image_path = frame_data
     cv2.imwrite(image_path, frame)
+
+def get_frame_types(video_path):
+    """ffprobe로 프레임 타입 가져오기"""
+    ffprobe_path = os.path.join(os.getcwd(), "utils\\ffprobe.exe")  # ffprobe.exe 경로
+    if not os.path.exists(ffprobe_path):
+        raise FileNotFoundError(f"ffprobe.exe not found in {os.getcwd()}")
+    
+    cmd = [
+        ffprobe_path,
+        "-i", video_path,
+        "-select_streams", "v:0",
+        "-show_frames",
+        "-show_entries", "frame=pict_type",
+        "-of", "csv"
+    ]
+    
+    result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    if result.returncode != 0:
+        print(f"Error occurred: {result.stderr}")
+        return []
+    
+    frames = [line.split(",")[-1] for line in result.stdout.splitlines() if line.startswith("frame")]
+    return frames
